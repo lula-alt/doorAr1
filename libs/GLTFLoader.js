01ab8a177c02f6e5c3ec2ecea657ebbcad2b4240
lula-alt <kanadiode@gmail.com>
2025-04-12 16:06:43 +0400

    Add files via upload

---
+++ libs/GLTFLoader.js
@@ -0,0 +1,2 @@
+
+export { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.175.0/examples/jsm/loaders/GLTFLoader.js';
---
+++ libs/three.module.js
@@ -0,0 +1,2 @@
+
+export * from 'https://cdn.jsdelivr.net/npm/three@0.175.0/build/three.module.js';
